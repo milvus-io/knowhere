0d58356edf31eb46262ec2e871fe1b712e5a6f13
Yuchen Gao <103244284@qq.com>
2022-01-07 10:42:32 +0800

    1) Refactor code structure. 2) Fix lint errors. 3) Remove demo test cases (To be re-added as future unit tests).

--- knowhere/knowhere/index/vector_index/impl/bruteforce/BruteForce.cpp
+++ knowhere/knowhere/index/vector_index/impl/bruteforce/BruteForce.cpp
@@ -1,5 +1,15 @@
-#include "BruteForce.h"
+// Copyright (C) 2019-2020 Zilliz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under the License
 #include <omp.h>
+#include "knowhere/index/vector_index/impl/bruteforce/BruteForce.h"
 
 namespace knowhere {
 
@@ -65,15 +75,15 @@ void knn_inner_product_sse(const float * x,
 
         for (size_t j = 0; j < ny; j++) {
             if (!bitset || !bitset.test(j)) {
-                float disij = faiss::fvec_inner_product_ref (x_i, y_j, d);
+                float disij = faiss::fvec_inner_product_ref(x_i, y_j, d);
                 if (disij > val_[0]) {
-                    minheap_swap_top (k, val_, ids_, disij, j);
+                    minheap_swap_top(k, val_, ids_, disij, j);
                 }
             }
             y_j += d;
         }
 
-        minheap_reorder (k, val_, ids_);
+        minheap_reorder(k, val_, ids_);
     }
 }
 
---
+++ knowhere/knowhere/index/vector_index/impl/bruteforce/SimpleIndexFlat.cpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2019-2020 Zilliz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under the License
+
+#include "knowhere/common/Heap.h"
+#include "knowhere/index/vector_index/impl/bruteforce/BruteForce.h"
+#include "knowhere/index/vector_index/impl/bruteforce/SimpleIndexFlat.h"
+
+namespace knowhere {
+
+SimpleIndexFlat::SimpleIndexFlat(idx_t d, MetricType metric) {
+    this->d = d;
+    this->metric_type = metric;
+}
+
+void SimpleIndexFlat::add(idx_t n, const float *x) {
+    xb.insert(xb.end(), x, x + n * d);
+    ntotal += n;
+}
+
+void SimpleIndexFlat::search(idx_t n, const float *x, idx_t k,
+                        float *distances, idx_t *labels,
+                        const faiss::BitsetView bitset) const {
+    // we see the distances and labels as heaps
+    if (metric_type == METRIC_INNER_PRODUCT) {
+        float_minheap_array_t res = {
+                size_t(n), size_t(k), labels, distances};
+        knn_inner_product_sse(x, xb.data(), d, n, ntotal, &res, bitset);
+    } else {
+        // metric_type == METRIC_L2
+        float_maxheap_array_t res = {
+                size_t(n), size_t(k), labels, distances};
+        knn_L2sqr_sse(x, xb.data(), d, n, ntotal, &res, bitset);
+    }
+}
+
+void SimpleIndexFlat::train(idx_t n, const float* x) {
+    // Do nothing.
+}
+
+
+} // namespace knowhere
