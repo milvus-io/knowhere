3776e6d424222d34edf22c1dcc0b8fbd08541b00
Yui <69466447+soothing-rain@users.noreply.github.com>
2022-01-11 16:28:50 +0800

    Support unit testing on MacOS and more. (#35)
    * Make unit test support macos.
    
    * 1) Refactor code structure.
    2) Fix lint errors.
    3) Remove demo test cases (To be re-added as future unit tests).
    
    * Sync up with lastest main and flip mac unit test CI.
    
    * Make build fail immediately and fix broken ubuntu unit tests.
    
    * Add unit tests for brute force search.
    
    * Add unit tests for brute force search.
    
    * Resolve warnings to fix ubuntu unit tests.
    
    * Guard int64_t by OS type and see if unit test can be fixed.
    
    * Add another guard.
    
    * Add more cases to unit test and fix failing unit test.
    
    * Re-struct build and cmake files.
    
    * Update cmake as "linux" is not supported by default.
    
    * Address comments.

--- knowhere/knowhere/index/vector_index/impl/bruteforce/BruteForce.cpp
+++ knowhere/knowhere/index/vector_index/impl/bruteforce/BruteForce.cpp
@@ -1,5 +1,21 @@
-#include "BruteForce.h"
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <omp.h>
+#include "knowhere/index/vector_index/impl/bruteforce/BruteForce.h"
 
 namespace knowhere {
 
@@ -65,15 +81,15 @@ void knn_inner_product_sse(const float * x,
 
         for (size_t j = 0; j < ny; j++) {
             if (!bitset || !bitset.test(j)) {
-                float disij = faiss::fvec_inner_product_ref (x_i, y_j, d);
+                float disij = faiss::fvec_inner_product_ref(x_i, y_j, d);
                 if (disij > val_[0]) {
-                    minheap_swap_top (k, val_, ids_, disij, j);
+                    minheap_swap_top(k, val_, ids_, disij, j);
                 }
             }
             y_j += d;
         }
 
-        minheap_reorder (k, val_, ids_);
+        minheap_reorder(k, val_, ids_);
     }
 }
 
