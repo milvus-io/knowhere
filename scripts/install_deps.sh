dd1a404de3d298800fd5eb427b12a69ccc4e1c2d
Cai Yudong <yudong.cai@zilliz.com>
2022-02-16 18:50:33 +0800

    Pre-install libopenblas-dev for ubuntu18.04 (#67)

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -35,7 +35,8 @@ if [[ "${MACHINE}" == "Linux"  ]]; then
         # for Ubuntu 18.04
         sudo apt install -y g++ gcc make ccache libssl-dev zlib1g-dev libboost-regex-dev \
             libboost-program-options-dev libboost-system-dev libboost-filesystem-dev \
-            libboost-serialization-dev python3-dev libboost-python-dev libcurl4-openssl-dev gfortran libtbb-dev
+            libboost-serialization-dev python3-dev libboost-python-dev libcurl4-openssl-dev \
+            gfortran libtbb-dev libopenblas-dev
     fi
 fi
 
