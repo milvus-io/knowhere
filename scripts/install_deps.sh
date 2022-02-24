44ae52b8d9bb79035bd5d641474100521abb1f48
Jenny Li <jing.li@zilliz.com>
2022-02-24 14:10:06 +0800

    Add centos7 support (#74)

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -43,6 +43,37 @@ if [[ "${MACHINE}" == "Linux" ]]; then
         if [[ "${INSTALL_OPENBLAS}" == "true" ]]; then
           sudo apt install -y libopenblas-dev
         fi
+    elif [[ -x "$(command -v yum)" ]]; then
+        # for CentOS 7
+        sudo yum install -y epel-release centos-release-scl-rh wget && \
+        sudo yum install -y git make automake ccache openssl-devel zlib-devel \
+            libcurl-devel python3-devel \
+            devtoolset-7-gcc devtoolset-7-gcc-c++ devtoolset-7-gcc-gfortran \
+            llvm-toolset-7.0-clang llvm-toolset-7.0-clang-tools-extra 
+
+        echo "source scl_source enable devtoolset-7" | sudo tee -a /etc/profile.d/devtoolset-7.sh
+        echo "source scl_source enable llvm-toolset-7.0" | sudo tee -a /etc/profile.d/llvm-toolset-7.sh
+        echo "export CLANG_TOOLS_PATH=/opt/rh/llvm-toolset-7.0/root/usr/bin" | sudo tee -a /etc/profile.d/llvm-toolset-7.sh
+        source "/etc/profile.d/llvm-toolset-7.sh"
+        #CMake 3.18 or higher is required
+        wget -c https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.tar.gz && \
+        tar -zxvf cmake-3.22.2-linux-x86_64.tar.gz && \
+        sudo ln -sf $(pwd)/cmake-3.22.2-linux-x86_64/bin/cmake /usr/bin/cmake
+
+        # Install tbb
+        # git clone https://github.com/wjakob/tbb.git && \
+        # cd tbb/build && \
+        # cmake .. && make -j && \
+        # sudo make install && \
+        # cd ../../ && rm -rf tbb/
+
+        # # Install boost
+        # wget -q https://boostorg.jfrog.io/artifactory/main/release/1.65.1/source/boost_1_65_1.tar.gz && \
+        #     tar zxf boost_1_65_1.tar.gz && cd boost_1_65_1 && \
+        #     ./bootstrap.sh --prefix=/usr/local --with-toolset=gcc --without-libraries=python && \
+        #     sudo ./b2 -j2 --prefix=/usr/local --without-python toolset=gcc install && \
+        #     cd ../ && rm -rf ./boost_1_65_1*
+
     fi
 fi
 
