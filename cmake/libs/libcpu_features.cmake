5358bcf27ba8564d2318952eb504f073dd438f45
Yusheng.Ma <yusheng.ma@zilliz.com>
2022-03-02 14:20:22 +0800

    format cmake

--- cmake/libs/libcpu_features.cmake
+++ cmake/libs/libcpu_features.cmake
@@ -18,10 +18,10 @@ ExternalProject_Add(
   BUILD_COMMAND ${MAKE_COMMAND}
   BUILD_BYPRODUCTS ${CPU_FEATURES_LIB})
 
-
 add_library(libcpu_features STATIC IMPORTED GLOBAL)
 add_dependencies(libcpu_features cpu_features)
 set_target_properties(
-    libcpu_features PROPERTIES IMPORTED_LOCATION ${CPU_FEATURES_LIB}
-    INTERFACE_INCLUDE_DIRECTORIES
-    ${CMAKE_SOURCE_DIR}/thirdparty/cpu_features/include)
+  libcpu_features
+  PROPERTIES IMPORTED_LOCATION ${CPU_FEATURES_LIB}
+             INTERFACE_INCLUDE_DIRECTORIES
+             ${CMAKE_SOURCE_DIR}/thirdparty/cpu_features/include)
