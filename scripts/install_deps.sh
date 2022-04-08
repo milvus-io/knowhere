6915618cedbd0359560b3af56e144eea8cf734b1
yudong.cai <yudong.cai@zilliz.com>
2022-04-08 20:21:51 +0800

    Remove boost and tbb from install_deps.sh
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -33,10 +33,7 @@ esac
 if [[ "${MACHINE}" == "Linux" ]]; then
     if [[ -x "$(command -v apt)" ]]; then
         # for Ubuntu 18.04
-        sudo apt install -y g++ gcc make ccache libssl-dev zlib1g-dev libboost-regex-dev \
-            libboost-program-options-dev libboost-system-dev libboost-filesystem-dev \
-            libboost-serialization-dev python3-dev libboost-python-dev libcurl4-openssl-dev \
-            gfortran libtbb-dev
+        sudo apt install -y g++ gcc make ccache libssl-dev zlib1g-dev python3-dev libcurl4-openssl-dev gfortran
         # Pre-installation of openblas can save about 15 minutes of openblas building time.
         # But the apt-installed openblas version is 0.2.20, while the latest openblas version is 0.3.19.
         # So we only pre-install openblas in Unittest, and compile openblas-0.3.19 when release.
@@ -59,24 +56,9 @@ if [[ "${MACHINE}" == "Linux" ]]; then
         wget -c https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.tar.gz && \
         tar -zxvf cmake-3.22.2-linux-x86_64.tar.gz && \
         sudo ln -sf $(pwd)/cmake-3.22.2-linux-x86_64/bin/cmake /usr/bin/cmake
-
-        # Install tbb
-        # git clone https://github.com/wjakob/tbb.git && \
-        # cd tbb/build && \
-        # cmake .. && make -j && \
-        # sudo make install && \
-        # cd ../../ && rm -rf tbb/
-
-        # # Install boost
-        # wget -q https://boostorg.jfrog.io/artifactory/main/release/1.65.1/source/boost_1_65_1.tar.gz && \
-        #     tar zxf boost_1_65_1.tar.gz && cd boost_1_65_1 && \
-        #     ./bootstrap.sh --prefix=/usr/local --with-toolset=gcc --without-libraries=python && \
-        #     sudo ./b2 -j2 --prefix=/usr/local --without-python toolset=gcc install && \
-        #     cd ../ && rm -rf ./boost_1_65_1*
-
     fi
 fi
 
 if [[ "${MACHINE}" == "Mac"  ]]; then
-    brew install boost libomp llvm ninja tbb openblas
+    brew install libomp llvm ninja openblas
 fi
