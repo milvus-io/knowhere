0697f1437852c87608efbac976b66846cde6f8c7
yudong.cai <yudong.cai@zilliz.com>
2022-05-12 15:55:19 +0800

    Optimize benchmark
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- unittest/benchmark/benchmark_knowhere_perf.cpp
+++ unittest/benchmark/benchmark_knowhere_perf.cpp
@@ -171,7 +171,7 @@ print_array(const char* header, bool is_integer, const void* arr, int32_t nq, in
 using idx_t = int64_t;
 using distance_t = float;
 
-class Benchmark_knowhere : public ::testing::Test {
+class Benchmark_knowhere_perf : public ::testing::Test {
  public:
     double get_time_diff() {
         return elapsed() - T0_;
@@ -206,11 +206,12 @@ class Benchmark_knowhere : public ::testing::Test {
         return true;
     }
 
-    int32_t CalcRecall(const idx_t* ids, int32_t nq, int32_t k) {
+    int32_t CalcRecall(const idx_t* ids, int32_t nq_start, int32_t step, int32_t k) {
+        assert(nq_start + step <= 10000);
         int32_t min_k = std::min(gt_k_, k);
         int32_t hit = 0;
-        for (int32_t i = 0; i < nq; i++) {
-            std::unordered_set<idx_t> ground(gt_ids_ + i * gt_k_, gt_ids_ + i * gt_k_ + min_k);
+        for (int32_t i = 0; i < step; i++) {
+            std::unordered_set<idx_t> ground(gt_ids_ + (i + nq_start) * gt_k_, gt_ids_ + (i + nq_start) * gt_k_ + min_k);
             for (int32_t j = 0; j < min_k; j++) {
                 idx_t id = ids[i * k + j];
                 if (ground.count(id) > 0) {
@@ -362,11 +363,12 @@ class Benchmark_knowhere : public ::testing::Test {
     void test_idmap(const knowhere::Config& cfg) {
         auto conf = cfg;
 
+        int32_t no = 0;
         printf("\n[%0.3f s] %s | %s \n",
                get_time_diff(), ann_test_name_.c_str(), std::string(index_type_).c_str());
         printf("================================================================================\n");
-        for (auto nq : NQs_) {
-            knowhere::DatasetPtr ds_ptr = knowhere::GenDataset(nq, dim_, xq_);
+        for (int32_t i = 0; i + NQ_STEP_ <= GT_NQ_; i = (i + NQ_STEP_) % GT_NQ_) {
+            knowhere::DatasetPtr ds_ptr = knowhere::GenDataset(NQ_STEP_, dim_, xq_ + (i * dim_));
             for (auto k : TOPKs_) {
                 knowhere::SetMetaTopk(conf, k);
 
@@ -375,9 +377,9 @@ class Benchmark_knowhere : public ::testing::Test {
                 t_end = elapsed();
 
                 auto ids = knowhere::GetDatasetIDs(result);
-                int32_t hit = CalcRecall(ids, nq, k);
-                printf("  nq = %4d, k = %4d, elapse = %.4fs, R@ = %.4f\n",
-                       nq, k, (t_end - t_start), (hit / float(nq * std::min(gt_k_, k))));
+                int32_t hit = CalcRecall(ids, i, NQ_STEP_, k);
+                printf("  No.%4d: nq = [%4d, %4d), k = %4d, elapse = %.4fs, R@ = %.4f\n",
+                       no++, i, i + NQ_STEP_, k, (t_end - t_start), (hit / float(NQ_STEP_ * std::min(gt_k_, k))));
             }
         }
         printf("================================================================================\n");
@@ -389,13 +391,14 @@ class Benchmark_knowhere : public ::testing::Test {
         auto conf = cfg;
         auto nlist = knowhere::GetIndexParamNlist(conf);
 
+        int32_t no = 0;
         printf("\n[%0.3f s] %s | %s | nlist=%ld\n",
                get_time_diff(), ann_test_name_.c_str(), std::string(index_type_).c_str(), nlist);
         printf("================================================================================\n");
-        for (auto nprobe : NPROBEs_) {
-            knowhere::SetIndexParamNprobe(conf, nprobe);
-            for (auto nq : NQs_) {
-                knowhere::DatasetPtr ds_ptr = knowhere::GenDataset(nq, dim_, xq_);
+        for (int32_t i = 0; i + NQ_STEP_ <= GT_NQ_; i = (i + NQ_STEP_) % GT_NQ_) {
+            knowhere::DatasetPtr ds_ptr = knowhere::GenDataset(NQ_STEP_, dim_, xq_ + (i * dim_));
+            for (auto nprobe : NPROBEs_) {
+                knowhere::SetIndexParamNprobe(conf, nprobe);
                 for (auto k : TOPKs_) {
                     knowhere::SetMetaTopk(conf, k);
 
@@ -404,9 +407,9 @@ class Benchmark_knowhere : public ::testing::Test {
                     t_end = elapsed();
 
                     auto ids = knowhere::GetDatasetIDs(result);
-                    int32_t hit = CalcRecall(ids, nq, k);
-                    printf("  nprobe = %4d, nq = %4d, k = %4d, elapse = %.4fs, R@ = %.4f\n",
-                           nprobe, nq, k, (t_end - t_start), (hit / float(nq * std::min(gt_k_, k))));
+                    int32_t hit = CalcRecall(ids, i, NQ_STEP_, k);
+                    printf("  No.%4d: nprobe = %4d, nq = [%4d, %4d), k = %4d, elapse = %.4fs, R@ = %.4f\n",
+                           no++, nprobe, i, i + NQ_STEP_, k, (t_end - t_start), (hit / float(NQ_STEP_ * std::min(gt_k_, k))));
                 }
             }
         }
@@ -420,13 +423,14 @@ class Benchmark_knowhere : public ::testing::Test {
         auto M = knowhere::GetIndexParamHNSWM(conf);
         auto efConstruction = knowhere::GetIndexParamEfConstruction(conf);
 
+        int32_t no = 0;
         printf("\n[%0.3f s] %s | %s | M=%ld | efConstruction=%ld\n",
                get_time_diff(), ann_test_name_.c_str(), std::string(index_type_).c_str(), M, efConstruction);
         printf("================================================================================\n");
-        for (auto ef: EFs_) {
-            knowhere::SetIndexParamEf(conf, ef);
-            for (auto nq : NQs_) {
-                knowhere::DatasetPtr ds_ptr = knowhere::GenDataset(nq, dim_, xq_);
+        for (int32_t i = 0; i + NQ_STEP_ <= GT_NQ_; i = (i + NQ_STEP_) % GT_NQ_) {
+            knowhere::DatasetPtr ds_ptr = knowhere::GenDataset(NQ_STEP_, dim_, xq_ + (i * dim_));
+            for (auto ef: EFs_) {
+                knowhere::SetIndexParamEf(conf, ef);
                 for (auto k : TOPKs_) {
                     knowhere::SetMetaTopk(conf, k);
 
@@ -435,9 +439,9 @@ class Benchmark_knowhere : public ::testing::Test {
                     t_end = elapsed();
 
                     auto ids = knowhere::GetDatasetIDs(result);
-                    int32_t hit = CalcRecall(ids, nq, k);
-                    printf("  ef = %4d, nq = %4d, k = %4d, elapse = %.4fs, R@ = %.4f\n",
-                           ef, nq, k, (t_end - t_start), (hit / float(nq * std::min(gt_k_, k))));
+                    int32_t hit = CalcRecall(ids, i, NQ_STEP_, k);
+                    printf("  No.%4d: ef = %4d, nq = [%4d, %4d), k = %4d, elapse = %.4fs, R@ = %.4f\n",
+                           no++, ef, i, i + NQ_STEP_, k, (t_end - t_start), (hit / float(NQ_STEP_ * std::min(gt_k_, k))));
                 }
             }
         }
@@ -450,13 +454,14 @@ class Benchmark_knowhere : public ::testing::Test {
         auto conf = cfg;
         auto n_trees = knowhere::GetIndexParamNtrees(conf);
 
+        int32_t no = 0;
         printf("\n[%0.3f s] %s | %s | n_trees=%ld \n",
                get_time_diff(), ann_test_name_.c_str(), std::string(index_type_).c_str(), n_trees);
         printf("================================================================================\n");
-        for (auto sk: SEARCH_Ks_) {
-            knowhere::SetIndexParamSearchK(conf, sk);
-            for (auto nq : NQs_) {
-                knowhere::DatasetPtr ds_ptr = knowhere::GenDataset(nq, dim_, xq_);
+        for (int32_t i = 0; i + NQ_STEP_ <= GT_NQ_; i = (i + NQ_STEP_) % GT_NQ_) {
+            knowhere::DatasetPtr ds_ptr = knowhere::GenDataset(NQ_STEP_, dim_, xq_ + (i * dim_));
+            for (auto sk: SEARCH_Ks_) {
+                knowhere::SetIndexParamSearchK(conf, sk);
                 for (auto k : TOPKs_) {
                     knowhere::SetMetaTopk(conf, k);
 
@@ -465,9 +470,9 @@ class Benchmark_knowhere : public ::testing::Test {
                     t_end = elapsed();
 
                     auto ids = knowhere::GetDatasetIDs(result);
-                    int32_t hit = CalcRecall(ids, nq, k);
-                    printf("  search_k = %4d, nq = %4d, k = %4d, elapse = %.4fs, R@ = %.4f\n",
-                           sk, nq, k, (t_end - t_start), (hit / float(nq * std::min(gt_k_, k))));
+                    int32_t hit = CalcRecall(ids, i, NQ_STEP_, k);
+                    printf("  No.%4d: search_k = %4d, nq = [%4d, %4d), k = %4d, elapse = %.4fs, R@ = %.4f\n",
+                           no++, sk, i, i + NQ_STEP_, k, (t_end - t_start), (hit / float(NQ_STEP_ * std::min(gt_k_, k))));
                 }
             }
         }
@@ -520,92 +525,88 @@ class Benchmark_knowhere : public ::testing::Test {
     knowhere::VecIndexPtr index_ = nullptr;
     knowhere::Config cfg_;
 
+    const int32_t GT_NQ_ = 10000;
+    const int32_t NQ_STEP_ = 10;
     const std::vector<int32_t> NQs_ = {10000};
     const std::vector<int32_t> TOPKs_ = {10};
 
     // IVF index params
-    const std::vector<int32_t> NLISTs_ = {1024};
-    const std::vector<int32_t> NPROBEs_ = {1, 2, 4, 8, 16, 32, 64, 128, 256};
+    const int32_t NLIST_ = 128;
+    const std::vector<int32_t> NPROBEs_ = {16};
 
     // HNSW index params
-    const std::vector<int32_t> Ms_ = {16};
-    const std::vector<int32_t> EFCONs_ = {100};
-    const std::vector<int32_t> EFs_ = {16, 32, 64, 128, 256};
+    const int32_t M_ = 16;
+    const int32_t EFCON_ = 100;
+    const std::vector<int32_t> EFs_ = {16};
 
     // ANNOY index params
-    const std::vector<int32_t> N_TREEs_ = {32};
-    const std::vector<int32_t> SEARCH_Ks_ = {16, 32, 64, 128, 256};
+    const int32_t N_TREE_ = 32;
+    const std::vector<int32_t> SEARCH_Ks_ = {16};
 };
 
-TEST_F(Benchmark_knowhere, TEST_IDMAP) {
+TEST_F(Benchmark_knowhere_perf, TEST_IDMAP) {
     index_type_ = knowhere::IndexEnum::INDEX_FAISS_IDMAP;
 
     knowhere::Config conf = cfg_;
     std::string index_file_name = get_index_name({});
+
     create_cpu_index(index_file_name, conf);
     index_->Load(binary_set_);
     test_idmap(conf);
 }
 
-TEST_F(Benchmark_knowhere, TEST_IVFFLAT_NM) {
+TEST_F(Benchmark_knowhere_perf, TEST_IVFFLAT_NM) {
     index_type_ = knowhere::IndexEnum::INDEX_FAISS_IVFFLAT;
 
     knowhere::Config conf = cfg_;
-    for (auto nlist : NLISTs_) {
-        std::string index_file_name = get_index_name({nlist});
-        knowhere::SetIndexParamNlist(conf, nlist);
-        create_cpu_index(index_file_name, conf);
+    knowhere::SetIndexParamNlist(conf, NLIST_);
+    std::string index_file_name = get_index_name({NLIST_});
 
-        // IVFFLAT_NM should load raw data
-        knowhere::BinaryPtr bin = std::make_shared<knowhere::Binary>();
-        bin->data = std::shared_ptr<uint8_t[]>((uint8_t*)xb_, [&](uint8_t*) {});
-        bin->size = dim_ * nb_ * sizeof(float);
-        binary_set_.Append(RAW_DATA, bin);
+    create_cpu_index(index_file_name, conf);
 
-        index_->Load(binary_set_);
-        test_ivf(conf);
-    }
+    // IVFFLAT_NM should load raw data
+    knowhere::BinaryPtr bin = std::make_shared<knowhere::Binary>();
+    bin->data = std::shared_ptr<uint8_t[]>((uint8_t*)xb_, [&](uint8_t*) {});
+    bin->size = dim_ * nb_ * sizeof(float);
+    binary_set_.Append(RAW_DATA, bin);
+
+    index_->Load(binary_set_);
+    test_ivf(conf);
 }
 
-TEST_F(Benchmark_knowhere, TEST_IVFSQ8) {
+TEST_F(Benchmark_knowhere_perf, TEST_IVFSQ8) {
     index_type_ = knowhere::IndexEnum::INDEX_FAISS_IVFSQ8;
 
     knowhere::Config conf = cfg_;
-    for (auto nlist : NLISTs_) {
-        std::string index_file_name = get_index_name({nlist});
-        knowhere::SetIndexParamNlist(conf, nlist);
-        create_cpu_index(index_file_name, conf);
-        index_->Load(binary_set_);
-        test_ivf(conf);
-    }
+    knowhere::SetIndexParamNlist(conf, NLIST_);
+    std::string index_file_name = get_index_name({NLIST_});
+
+    create_cpu_index(index_file_name, conf);
+    index_->Load(binary_set_);
+    test_ivf(conf);
 }
 
-TEST_F(Benchmark_knowhere, TEST_HNSW) {
+TEST_F(Benchmark_knowhere_perf, TEST_HNSW) {
     index_type_ = knowhere::IndexEnum::INDEX_HNSW;
 
     knowhere::Config conf = cfg_;
-    for (auto M : Ms_) {
-        knowhere::SetIndexParamHNSWM(conf, M);
-        for (auto efc : EFCONs_) {
-            std::string index_file_name = get_index_name({M, efc});
-            knowhere::SetIndexParamEfConstruction(conf, efc);
-            create_cpu_index(index_file_name, conf);
-            index_->Load(binary_set_);
-            test_hnsw(conf);
-        }
-    }
+    knowhere::SetIndexParamHNSWM(conf, M_);
+    knowhere::SetIndexParamEfConstruction(conf, EFCON_);
+    std::string index_file_name = get_index_name({M_, EFCON_});
+
+    create_cpu_index(index_file_name, conf);
+    index_->Load(binary_set_);
+    test_hnsw(conf);
 }
 
-TEST_F(Benchmark_knowhere, TEST_ANNOY) {
-
+TEST_F(Benchmark_knowhere_perf, TEST_ANNOY) {
     index_type_ = knowhere::IndexEnum::INDEX_ANNOY;
 
     knowhere::Config conf = cfg_;
-    for (auto n : N_TREEs_) {
-        knowhere::SetIndexParamNtrees(conf, n);
-        std::string index_file_name = get_index_name({n});
-        create_cpu_index(index_file_name, conf);
-        index_->Load(binary_set_);
-        test_annoy(conf);
-    }
+    knowhere::SetIndexParamNtrees(conf, N_TREE_);
+    std::string index_file_name = get_index_name({N_TREE_});
+
+    create_cpu_index(index_file_name, conf);
+    index_->Load(binary_set_);
+    test_annoy(conf);
 }
