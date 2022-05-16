3db844d251ae844e702be2bdbbca8e603a9f1b44
yudong.cai <yudong.cai@zilliz.com>
2022-05-16 18:10:41 +0800

    Let GPU index call CPU range search
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- knowhere/index/vector_index/gpu/IndexGPUIVF.cpp
+++ knowhere/index/vector_index/gpu/IndexGPUIVF.cpp
@@ -81,11 +81,16 @@ GPUIVF::CopyGpuToGpu(const int64_t device_id, const Config& config) {
 
 int64_t
 GPUIVF::Size() {
-    if (!index_) {
-        KNOWHERE_THROW_MSG("index not initialize");
-    }
-    auto ivf_index = CopyGpuToCpu(Config());
-    return ivf_index->Size();
+    auto host_index = CopyGpuToCpu(Config());
+    return host_index->Size();
+}
+
+DatasetPtr
+GPUIVF::QueryByRange(const DatasetPtr& dataset,
+                     const Config& config,
+                     const faiss::BitsetView bitset) {
+    auto host_index = CopyGpuToCpu(Config());
+    return host_index->QueryByRange(dataset, config, bitset);
 }
 
 BinarySet
