4b8021d401a14cc721eb8f9f327ddbcba33c158e
Jenny Li <jing.li@zilliz.com>
2022-08-02 21:32:00 +0800

    [skip e2e]Add deps for diskAnn
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- ci/docker/ubuntu/scripts/deps/debug.sh
+++ ci/docker/ubuntu/scripts/deps/debug.sh
@@ -2,6 +2,7 @@ CMAKE_VERSION="v3.23"
 CMAKE_TAR="cmake-3.23.0-linux-x86_64.tar.gz"
 apt-get update && apt-get install -y --no-install-recommends wget curl g++ gcc ca-certificates \
 make ccache python3-dev gfortran python3-setuptools swig libopenblas-dev pip git vim \
+libaio-dev libgoogle-perftools-dev clang-format libboost-all-dev libmkl-full-dev \
 && apt-get remove --purge -y  \
 && rm -rf /var/lib/apt/lists/* \
 && cd /tmp && wget  --tries=3 --retry-connrefused "https://cmake.org/files/${CMAKE_VERSION}/${CMAKE_TAR}" \
