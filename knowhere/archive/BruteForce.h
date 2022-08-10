023fcf49899c20c9dc948ca32cb3308815c0dffa
yudong.cai <yudong.cai@zilliz.com>
2022-08-10 20:53:41 +0800

    Update interface BruteForceSearch() and BruteForceRangeSearch()
    Signed-off-by: yudong.cai <yudong.cai@zilliz.com>

--- knowhere/archive/BruteForce.h
+++ knowhere/archive/BruteForce.h
@@ -14,68 +14,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/** BruteForce provides brute-force search as an option that the data is just
- *  copied to the index without further encoding or organization.
- */
 #pragma once
 
-#include "knowhere/index/vector_index/helpers/IndexParameter.h"
+#include "knowhere/common/Dataset.h"
+#include "knowhere/common/Config.h"
 #include "knowhere/utils/BitsetView.h"
 
 namespace knowhere {
 
-/** knowhere wrapper API to call faiss brute force search for all metric types
- *
- * @param metric_type
- * @param xb            training vecors, size nb * dim
- * @param xq            query vecors, size nq * dim
- * @param dim
- * @param nb            rows of training vectors
- * @param nq            rows of query vectors
- * @param topk
- * @param labels        output, memory allocated and freed by caller
- * @param distances     output, memory allocated and freed by coller
- * @param bitset
- */
-void
-BruteForceSearch(
-    const knowhere::MetricType& metric_type,
-    const void* xb,
-    const void* xq,
-    const int64_t dim,
-    const int64_t nb,
-    const int64_t nq,
-    const int64_t topk,
-    int64_t* labels,
-    float* distances,
-    const faiss::BitsetView bitset);
+class BruteForce {
+ public:
+    static DatasetPtr
+    Search(const DatasetPtr base_dataset,
+           const DatasetPtr query_dataset,
+           const Config& config,
+           const faiss::BitsetView bitset);
 
-/** knowhere wrapper API to call faiss brute force range search for all metric types
- *
- * @param metric_type
- * @param xb            training vecors, size nb * dim
- * @param xq            query vecors, size nq * dim
- * @param dim
- * @param nb            rows of training vectors
- * @param nq            rows of query vectors
- * @param radius        range search radius
- * @param labels        output, memory allocated inside and freed by caller
- * @param distances     output, memory allocated inside and freed by coller
- * @param lims          output, memory allocated inside and freed by coller
- * @param bitset
- */
-void
-BruteForceRangeSearch(
-    const knowhere::MetricType& metric_type,
-    const void* xb,
-    const void* xq,
-    const int64_t dim,
-    const int64_t nb,
-    const int64_t nq,
-    const float radius,
-    int64_t*& labels,
-    float*& distances,
-    size_t*& lims,
-    const faiss::BitsetView bitset);
+    static DatasetPtr
+    RangeSearch(const DatasetPtr base_dataset,
+                const DatasetPtr query_dataset,
+                const Config& config,
+                const faiss::BitsetView bitset);
+};
 
 }  // namespace knowhere
