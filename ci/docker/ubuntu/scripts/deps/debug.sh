b67387a77c0e250ca33009d15e0b6040218c4cc9
Jenny Li <jing.li@zilliz.com>
2022-08-25 11:18:17 +0800

    [skip e2e]Remove mkl for the base image
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- ci/docker/ubuntu/scripts/deps/debug.sh
+++ ci/docker/ubuntu/scripts/deps/debug.sh
@@ -3,7 +3,7 @@ CMAKE_TAR="cmake-3.23.0-linux-x86_64.tar.gz"
 HDF_VERSION="1.13.2"
 apt-get update && apt-get install -y --no-install-recommends wget curl g++ gcc ca-certificates \
 make ccache python3-dev gfortran python3-setuptools swig libopenblas-dev pip git vim \
-libaio-dev libgoogle-perftools-dev clang-format libboost-all-dev libmkl-full-dev \
+libaio-dev libgoogle-perftools-dev clang-format libboost-all-dev  \
 && apt-get remove --purge -y  \
 && rm -rf /var/lib/apt/lists/* \
 && cd /tmp && wget  --tries=3 --retry-connrefused "https://cmake.org/files/${CMAKE_VERSION}/${CMAKE_TAR}" \
--- ci/docker/ubuntu/scripts/deps/e2e.sh
+++ ci/docker/ubuntu/scripts/deps/e2e.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get install -y --no-install-recommends wget curl g++ gcc ca-certificates \
  python3-dev gfortran python3-setuptools swig libopenblas-dev pip git vim \
- libmkl-dev libaio-dev libboost-all-dev \
+ libaio-dev libboost-all-dev \
 && apt-get remove --purge -y  \
 && rm -rf /var/lib/apt/lists/* 
