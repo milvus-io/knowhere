76843484206c417b95c380f5a2b3ce8f811477ad
Jenny Li <jing.li@zilliz.com>
2022-09-13 15:24:48 +0800

    [skip e2e] Fix e2e image
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- ci/docker/ubuntu/scripts/deps/e2e.sh
+++ ci/docker/ubuntu/scripts/deps/e2e.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install -y --no-install-recommends wget curl g++ gcc ca-certificates \
+apt-get update && apt-get install -y --no-install-recommends wget curl g++ gcc make ca-certificates \
  python3-dev gfortran python3-setuptools swig libopenblas-dev pip git vim \
  libaio-dev libboost-all-dev \
 && apt-get remove --purge -y  \
