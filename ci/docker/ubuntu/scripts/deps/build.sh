4d10334d88258436d1336f7fcbeb9b8b896cc44a
Jenny Li <jing.li@zilliz.com>
2022-09-13 16:34:30 +0800

    [skip e2e] Remove debug image (#467)
    Signed-off-by: Jenny Li <jing.li@zilliz.com>
    
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- ci/docker/ubuntu/scripts/deps/build.sh
+++ ci/docker/ubuntu/scripts/deps/build.sh
@@ -2,7 +2,7 @@ CMAKE_VERSION="v3.23"
 CMAKE_TAR="cmake-3.23.0-linux-x86_64.tar.gz"
 apt-get update && apt-get install -y --no-install-recommends wget curl g++ gcc ca-certificates \
 make ccache python3-dev gfortran python3-setuptools swig pip \
-libaio-dev libgoogle-perftools-dev clang-format libboost-all-dev \
+libaio-dev libgoogle-perftools-dev clang-format libboost-all-dev liblapack-dev \
 && apt-get remove --purge -y  \
 && rm -rf /var/lib/apt/lists/* \
 && wget https://github.com/xianyi/OpenBLAS/archive/v0.3.21.tar.gz && \
