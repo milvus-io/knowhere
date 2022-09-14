bf6f19420a580952da54269e27e137aa7f64e2b5
Yusheng.Ma <Yusheng.Ma@zilliz.com>
2022-09-14 06:43:38 +0000

    optimize openblas compile in pod
    Signed-off-by: Yusheng.Ma <Yusheng.Ma@zilliz.com>

--- ci/docker/ubuntu/scripts/deps/e2e.sh
+++ ci/docker/ubuntu/scripts/deps/e2e.sh
@@ -1,12 +1,13 @@
 apt-get update && apt-get install -y --no-install-recommends wget curl g++ gcc make ca-certificates \
- python3-dev gfortran python3-setuptools swig libopenblas-dev liblapack-dev pip git vim \
+ python3-dev gfortran python3-setuptools swig liblapack-dev pip git vim \
  libaio-dev libboost-all-dev \
 && apt-get remove --purge -y  \
 && rm -rf /var/lib/apt/lists/* \
 && wget https://github.com/xianyi/OpenBLAS/archive/v0.3.21.tar.gz && \
 tar zxvf v0.3.21.tar.gz && cd OpenBLAS-0.3.21 && \
 make NO_STATIC=1 NO_LAPACK=1 NO_LAPACKE=1 NO_CBLAS=1 NO_AFFINITY=1 USE_OPENMP=1 \
-    CFLAGS="-O3 -fPIC" TARGET=CORE2 DYNAMIC_ARCH=1 \
+    TARGET=HASWELL DYNAMIC_ARCH=1 \
     NUM_THREADS=64 MAJOR_VERSION=3 libs shared && \
-make -j4 PREFIX=/usr NO_STATIC=1 install && \
+make PREFIX=/usr/local NUM_THREADS=64 MAJOR_VERSION=3 install && \
+rm -f /usr/local/include/cblas.h /usr/local/include/lapack* && \
 cd .. && rm -rf OpenBLAS-0.3.21 && rm v0.3.21.tar.gz 
