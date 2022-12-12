61fa4e55688239e33ab8deb1bdf6aa0700e87c45
Yusheng.Ma <Yusheng.Ma@zilliz.com>
2022-12-12 09:59:51 +0000

    zero copy when use brute_force
    Signed-off-by: Yusheng.Ma <Yusheng.Ma@zilliz.com>

--- src/common/comp/brute_force.cc
+++ src/common/comp/brute_force.cc
@@ -105,6 +105,76 @@ BruteForce::Search(const DataSetPtr base_dataset, const DataSetPtr query_dataset
     return results;
 }
 
+Status
+BruteForce::SearchWithBuf(const DataSetPtr base_dataset, const DataSetPtr query_dataset, int64_t* ids, float* dis,
+                          const Json& config, const BitsetView& bitset) {
+    auto xb = base_dataset->GetTensor();
+    auto nb = base_dataset->GetRows();
+    auto dim = base_dataset->GetDim();
+
+    auto xq = query_dataset->GetTensor();
+    auto nq = query_dataset->GetRows();
+
+    BruteForceConfig cfg;
+    Config::Load(cfg, config, knowhere::SEARCH);
+
+    auto metric_type = Str2FaissMetricType(cfg.metric_type);
+    if (!metric_type.has_value()) {
+        return Status::invalid_metric_type;
+    }
+
+    int topk = cfg.k;
+    auto labels = ids;
+    auto distances = dis;
+
+    auto faiss_metric_type = metric_type.value();
+    switch (faiss_metric_type) {
+        case faiss::METRIC_L2: {
+            faiss::float_maxheap_array_t buf{(size_t)nq, (size_t)topk, labels, distances};
+            faiss::knn_L2sqr((const float*)xq, (const float*)xb, dim, nq, nb, &buf, nullptr, bitset);
+            break;
+        }
+        case faiss::METRIC_INNER_PRODUCT: {
+            faiss::float_minheap_array_t buf{(size_t)nq, (size_t)topk, labels, distances};
+            faiss::knn_inner_product((const float*)xq, (const float*)xb, dim, nq, nb, &buf, bitset);
+            break;
+        }
+        case faiss::METRIC_Jaccard:
+        case faiss::METRIC_Tanimoto: {
+            faiss::float_maxheap_array_t res = {size_t(nq), size_t(topk), labels, distances};
+            binary_distance_knn_hc(faiss::METRIC_Jaccard, &res, (const uint8_t*)xq, (const uint8_t*)xb, nb, dim / 8,
+                                   bitset);
+
+            if (faiss_metric_type == faiss::METRIC_Tanimoto) {
+                for (int i = 0; i < topk * nq; i++) {
+                    distances[i] = faiss::Jaccard_2_Tanimoto(distances[i]);
+                }
+            }
+            break;
+        }
+        case faiss::METRIC_Hamming: {
+            std::vector<int32_t> int_distances(nq * topk);
+            faiss::int_maxheap_array_t res = {size_t(nq), size_t(topk), labels, int_distances.data()};
+            binary_distance_knn_hc(faiss::METRIC_Hamming, &res, (const uint8_t*)xq, (const uint8_t*)xb, nb, dim / 8,
+                                   bitset);
+            for (int i = 0; i < nq * topk; ++i) {
+                distances[i] = int_distances[i];
+            }
+            break;
+        }
+        case faiss::METRIC_Substructure:
+        case faiss::METRIC_Superstructure: {
+            // only matched ids will be chosen, not to use heap
+            binary_distance_knn_mc(faiss_metric_type, (const uint8_t*)xq, (const uint8_t*)xb, nq, nb, topk, dim / 8,
+                                   distances, labels, bitset);
+            break;
+        }
+        default:
+            return Status::invalid_metric_type;
+    }
+    return Status::success;
+}
+
 /** knowhere wrapper API to call faiss brute force range search for all metric types
  */
 expected<DataSetPtr, Status>
