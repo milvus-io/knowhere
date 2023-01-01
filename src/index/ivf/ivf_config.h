0f17fd58829edc5f8b8c3c0e97be645ea8fbcc12
Cai Yudong <yudong.cai@zilliz.com>
2023-01-01 17:28:09 +0800

    Fix range search result error (#618)

--- src/index/ivf/ivf_config.h
+++ src/index/ivf/ivf_config.h
@@ -14,7 +14,8 @@ class IvfConfig : public BaseConfig {
         KNOWHERE_CONFIG_DECLARE_FIELD(nprobe)
             .set_default(1024)
             .description("number of probes at query time.")
-            .for_search();
+            .for_search()
+            .for_range_search();
     }
 };
 
