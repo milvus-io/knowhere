67dbf28ccb569c16badc87bde5ea282b3941d521
Cai Yudong <yudong.cai@zilliz.com>
2023-01-01 17:28:09 +0800

    Fix range search result error (#618)

--- src/index/hnsw/hnsw_config.h
+++ src/index/hnsw/hnsw_config.h
@@ -13,7 +13,7 @@ class HnswConfig : public BaseConfig {
     KNOHWERE_DECLARE_CONFIG(HnswConfig) {
         KNOWHERE_CONFIG_DECLARE_FIELD(M).description("hnsw M").set_default(16).for_train();
         KNOWHERE_CONFIG_DECLARE_FIELD(efConstruction).description("hnsw efConstruction").set_default(200).for_train();
-        KNOWHERE_CONFIG_DECLARE_FIELD(ef).description("hnsw ef").set_default(32).for_search();
+        KNOWHERE_CONFIG_DECLARE_FIELD(ef).description("hnsw ef").set_default(32).for_search().for_range_search();
         KNOWHERE_CONFIG_DECLARE_FIELD(overview_levels)
             .description("hnsw overview levels for feder")
             .set_default(3)
