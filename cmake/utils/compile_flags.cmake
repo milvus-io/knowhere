c69b44d6b7180ce5731a9916c497981af967beca
Yudong Cai <yudong.cai@zilliz.com>
2023-02-17 14:34:28 +0800

    Use -O0 for debug build
    Signed-off-by: Yudong Cai <yudong.cai@zilliz.com>

--- cmake/utils/compile_flags.cmake
+++ cmake/utils/compile_flags.cmake
@@ -17,16 +17,15 @@ endif()
 
 if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
   message(STATUS "Build in Debug mode")
-  set(CMAKE_CXX_FLAGS "-O1 -g -Wall -fPIC ${CMAKE_CXX_FLAGS}")
+  set(CMAKE_CXX_FLAGS "-O0 -g -Wall -fPIC ${CMAKE_CXX_FLAGS}")
   if(USE_CUDA)
     set(CMAKE_CUDA_FLAGS
-        "-O1 -g -Xcompiler=-w -Xcompiler=-fPIC ${CMAKE_CUDA_FLAGS}")
+        "-O0 -g -Xcompiler=-w -Xcompiler=-fPIC ${CMAKE_CUDA_FLAGS}")
   endif()
 else()
   set(CMAKE_CXX_FLAGS "-O2 -Wall -fPIC ${CMAKE_CXX_FLAGS}")
-
   if(USE_CUDA)
     set(CMAKE_CUDA_FLAGS
-        "-O2  -Xcompiler=-w -Xcompiler=-fPIC ${CMAKE_CUDA_FLAGS}")
+        "-O2 -Xcompiler=-w -Xcompiler=-fPIC ${CMAKE_CUDA_FLAGS}")
   endif()
 endif()
