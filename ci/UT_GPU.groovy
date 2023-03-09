09dc3c446e9568ef8416ae03dd1d39df3c12e875
Bennu-Li <yunmei.li@zilliz.com>
2023-03-09 11:04:22 +0800

    Update gpu-ut groovy
    Signed-off-by: Bennu-Li <yunmei.li@zilliz.com>

--- ci/UT_GPU.groovy
+++ ci/UT_GPU.groovy
@@ -32,7 +32,6 @@ pipeline {
                         sh "git config --global --add safe.directory '*'"
                         sh "git submodule update --recursive --init"
                         sh "mkdir build"
-                        sh "export ASAN_OPTIONS=protect_shadow_gap=0"
                         sh "cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_DISKANN=ON -DWITH_UT=ON -DWITH_ASAN=ON -DUSE_CUDA=ON \
                               && make -j8 \
                               && ./tests/ut/knowhere_tests"
