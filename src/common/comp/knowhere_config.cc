ccf7199d8be1d990ba9646439273718324117ebd
Yudong Cai <yudong.cai@zilliz.com>
2023-03-14 18:21:05 +0800

    Update GPU knowhere version
    Signed-off-by: Yudong Cai <yudong.cai@zilliz.com>

--- src/common/comp/knowhere_config.cc
+++ src/common/comp/knowhere_config.cc
@@ -29,7 +29,11 @@ KnowhereConfig::ShowVersion() {
 #define XSTR(x) STR(x)
 #define STR(x) #x
 #ifdef KNOWHERE_VERSION
+#ifdef USE_CUDA
+    LOG_KNOWHERE_INFO_ << "Knowhere Version: " << XSTR(KNOWHERE_VERSION) << "-gpu";
+#else
     LOG_KNOWHERE_INFO_ << "Knowhere Version: " << XSTR(KNOWHERE_VERSION);
+#endif
 #else
     LOG_KNOWHERE_INFO_ << "Knowhere Version: unknown";
 #endif
