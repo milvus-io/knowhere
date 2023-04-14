a79da7bbd7df3736c4f02263c0803ba02b07338a
Yusheng.Ma <Yusheng.Ma@zilliz.com>
2023-04-14 10:09:01 +0000

    raft index, fix param m and add env for setting gpu mem pool size
    Signed-off-by: Yusheng.Ma <Yusheng.Ma@zilliz.com>

---
+++ src/index/ivf_raft/set_pool.h
@@ -0,0 +1,11 @@
+#ifndef RAFT_IVF_SET_POOL_H
+#define RAFT_IVF_SET_POOL_H
+
+#include <cstdio>
+namespace knowhere {
+void
+SetRaftMemPool(size_t init_size, size_t max_size);
+
+}
+
+#endif /* SET_POOL_H */
