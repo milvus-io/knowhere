a4c488d3b7dffb0a925d278899edfba177450342
Yudong Cai <yudong.cai@zilliz.com>
2023-05-25 14:59:55 +0800

    Update release.yaml and install_deps.sh
    Signed-off-by: Yudong Cai <yudong.cai@zilliz.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -34,16 +34,56 @@ if [[ "${MACHINE}" == "Linux" ]]; then
     if [[ -x "$(command -v apt)" ]]; then
         # for Ubuntu 18.04
         release_num=$(lsb_release -r --short)
-        sudo apt install -y g++ gcc make ccache python3-dev gfortran libcurl4-openssl-dev
+        ubuntu_alias="bionic"
+        sudo apt install -y g++ gcc make ccache python3 python3-pip gfortran libcurl4-openssl-dev libaio-dev lcov
         if [ "$release_num" == "20.04" ];then
             sudo apt install -y python3-setuptools swig
+            ubuntu_alias="focal"
         fi
+        #DiskANN dependencies
+        sudo apt-get install -y libboost-program-options-dev
+        sudo apt-get install -y libaio-dev
         pip3 install conan==1.58.0
+
         # Pre-installation of openblas can save about 15 minutes of openblas building time.
         # But the apt-installed openblas version is 0.2.20, while the latest openblas version is 0.3.19.
         # So we only pre-install openblas in Unittest, and compile openblas-0.3.19 when release.
-        if [[ "${INSTALL_OPENBLAS}" == "true" ]]; then
-          sudo apt install -y libopenblas-dev
+        if [[ "${INSTALL_OPENBLAS}" == "true" ]]
+          then
+            sudo apt install -y libopenblas-dev
+          else
+            wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee \
+            /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null \
+            && echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ '$ubuntu_alias' main' | sudo tee \
+            /etc/apt/sources.list.d/kitware.list >/dev/null \
+            && apt update && apt install -y cmake \
+            && wget https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v3.10.1.tar.gz \
+            && tar zxvf v3.10.1.tar.gz && cd lapack-3.10.1/  \
+            && cmake -B build -S . -DCMAKE_SKIP_RPATH=ON -DBUILD_SHARED_LIBS=ON \
+            -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/usr/local \
+            -DCMAKE_Fortran_COMPILER=gfortran -DLAPACKE_WITH_TMG=ON -DCBLAS=OFF \
+            -DBUILD_DEPRECATED=OFF \
+            && cmake --build build \
+            && sudo cmake --install build \
+            && sudo rm -r /usr/local/lib/libblas.* \
+            && sudo rm -r /usr/local/lib/liblapacke.* \
+            && sudo rm -r /usr/local/lib/pkgconfig/blas.* \
+            && sudo rm -r /usr/local/lib/pkgconfig/lapacke.* \
+            && sudo rm -r /usr/local/lib/cmake/lapacke* \
+            && sudo rm -r /usr/local/include/* \
+            && cd .. && rm -rf lapack-3.10.1 && rm v3.10.1.tar.gz
+
+            wget https://github.com/xianyi/OpenBLAS/archive/refs/tags/v0.3.21.tar.gz && \
+            tar zxvf v0.3.21.tar.gz && cd OpenBLAS-0.3.21 && \
+            make NO_STATIC=1 NO_LAPACK=1 NO_LAPACKE=1 NO_AFFINITY=1 USE_OPENMP=1 \
+            TARGET=HASWELL DYNAMIC_ARCH=1 \
+            NUM_THREADS=64 MAJOR_VERSION=3 libs shared && \
+            sudo make PREFIX=/usr/local NUM_THREADS=64 MAJOR_VERSION=3 install && \
+            sudo rm -f /usr/local/include/lapack* && \
+            sudo ln -s /usr/local/lib/libopenblasp-r0.3.21.so /lib/libblas.so && \
+            sudo ln -s /usr/local/lib/libopenblasp-r0.3.21.so /lib/libblas.so.3 && \
+            sudo ln -s /usr/local/lib/pkgconfig/openblas.pc /usr/local/lib/pkgconfig/blas.pc && \
+            cd .. && rm -rf OpenBLAS-0.3.21 && rm v0.3.21.tar.gz
         fi
     elif [[ -x "$(command -v yum)" ]]; then
         # for CentOS 7
@@ -56,6 +96,9 @@ if [[ "${MACHINE}" == "Linux" ]]; then
         echo "source scl_source enable llvm-toolset-7.0" | sudo tee -a /etc/profile.d/llvm-toolset-7.sh
         echo "export CLANG_TOOLS_PATH=/opt/rh/llvm-toolset-7.0/root/usr/bin" | sudo tee -a /etc/profile.d/llvm-toolset-7.sh
         source "/etc/profile.d/llvm-toolset-7.sh"
+        #DiskANN dependencies
+        sudo yum -y install boost-program-options
+        sudo yum -y install boost libaio
         #CMake 3.18 or higher is required
         wget -c https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2-linux-x86_64.tar.gz && \
         tar -zxvf cmake-3.22.2-linux-x86_64.tar.gz && \
